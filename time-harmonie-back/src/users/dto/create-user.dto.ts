59d3d9c755c12eeb2a3cf9c23079d3aeb6c9952d
Julien Dabadie <j.dabadie81@gmail.com>
2025-06-07 10:31:39 +0200

    feat: install, docker + docker compose

--- time-harmonie-back/src/users/dto/create-user.dto.ts
+++ time-harmonie-back/src/users/dto/create-user.dto.ts
@@ -1,1 +1,35 @@
-export class CreateUserDto {}
+import { ApiProperty } from '@nestjs/swagger';
+import { IsBoolean, IsDate, IsEmail, IsString } from 'class-validator';
+
+export class CreateUserDto {
+  @ApiProperty({ description: 'First name of the user' })
+  @IsString()
+  firstname: String;
+
+  @ApiProperty({ description: 'Last name of the user' })
+  @IsString()
+  lastname: String;
+
+  @ApiProperty({ description: 'Email of the user' })
+  @IsEmail()
+  email: String;
+
+  @ApiProperty({ description: 'Password of the user' })
+  @IsString()
+  password: String;
+
+  @ApiProperty({ description: 'Role of the user' })
+  @IsString()
+  role: String;
+
+  @ApiProperty({ description: 'Email verified of the user' })
+  @IsBoolean()
+  email_verified: boolean;
+  
+  @ApiProperty({
+    description: 'Date de création du compte',
+    example: '2024-06-01T12:00:00Z',
+  })
+  @IsDate()
+  created_at: Date;
+}
--- time-harmonie-back/src/users/entities/user.entity.ts
+++ time-harmonie-back/src/users/entities/user.entity.ts
@@ -1,1 +1,39 @@
-export class User {}
+import { ApiProperty } from '@nestjs/swagger';
+
+export class User {
+  @ApiProperty({
+    description: "Identifiant unique de l'utilisateur",
+    example: 'uuid',
+  })
+  id: string;
+
+  @ApiProperty({ description: "Prénom de l'utilisateur", example: 'John' })
+  firstname: string;
+
+  @ApiProperty({ description: "Nom de l'utilisateur", example: 'Doe' })
+  lastname: string;
+
+  @ApiProperty({
+    description: "Email de l'utilisateur",
+    example: 'john.doe@example.com',
+  })
+  email: string;
+
+  @ApiProperty({
+    description: "Mot de passe de l'utilisateur",
+    example: 'password',
+  })
+  password: string;
+
+  @ApiProperty({ description: "Rôle de l'utilisateur", example: 'user' })
+  role: string;
+
+  @ApiProperty({ description: "Email vérifié de l'utilisateur", example: true })
+  email_verified: boolean;
+  
+  @ApiProperty({
+    description: 'Date de création du compte',
+    example: '2024-06-01T12:00:00Z',
+  })
+  created_at: Date;
+}
